9a2f223fd05de2712da2ab0cedee68802b35d83c
chixx <lucottejeremie@gmail.com>
2024-05-11 13:09:31 +0200

    add folder test and file SecureVault.t.sol

---
+++ Backend/test/SecureVault.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+import {Proxy} from "../src/Proxy.sol";
+import {SecureVaultLogic} from "../src/SecureVaultLogic.sol";
+import {Factory} from "../src/Factory.sol";
+
+contract TestSecureVault is Test {
+  Proxy internal proxy;
+  SecureVaultLogic internal logic;
+  Factory internal factory;
+
+  uint256 internal ownerPrivateKey;
+  address internal owner;
+  uint256 internal user1PrivateKey;
+  address internal user1;
+  uint256 internal user2PrivateKey;
+  address internal user2;
+  uint256 internal user3PrivateKey;
+  address internal user3;
+
+  function setUp() public {
+    ownerPrivateKey = 0xA11CE;
+    owner = vm.addr(ownerPrivateKey);
+    user1PrivateKey = 0xB0B;
+    user1 = vm.addr(user1PrivateKey);
+    user2PrivateKey = 0xFE55E;
+    user2 = vm.addr(user2PrivateKey);
+    user3PrivateKey = 0xD1C;
+    user3 = vm.addr(user3PrivateKey);
+
+    vm.startPrank(owner);
+
+    factory = new Factory();
+    logic = new SecureVaultLogic(address(factory));
+    factory.setContractLogic(address(logic));
+  }
+}
