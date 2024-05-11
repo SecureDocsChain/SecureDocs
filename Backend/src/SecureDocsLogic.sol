2703bef2994f0ecdd9b5ccb86e62f4b2926a6242
chixx <lucottejeremie@gmail.com>
2024-05-11 12:13:48 +0200

    add folder src with file SecureDocsLogic.sol: mint , _update, tokenURI & initialize

---
+++ Backend/src/SecureDocsLogic.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+error Unauthorized();
+error AlreadyInitialized();
+
+struct Metadata {
+  uint8 visibility;
+  uint256 timestamp;
+  bytes32 documentHash;
+  bytes32[] keywords;
+  string documentType;
+  string uri;
+}
+
+contract SecureVaultLogic is ERC721, Ownable {
+  uint8 initialized;
+  uint256 ptrTokenId = 1;
+
+  mapping(uint256 => Metadata) public metadata;
+
+  constructor(address factory) ERC721("SecureVault", "SV") Ownable(factory) {}
+
+  // Initialize the contract with the provided parameters
+  function initialize(address initialOwner) external onlyOwner {
+    if (initialized == 1) revert AlreadyInitialized();
+    transferOwnership(initialOwner);
+    initialized = 1;
+  }
+
+  // Mint a new token with the provided metadata
+  function mint(
+    address to,
+    uint8 visibility,
+    bytes32 documentHash,
+    bytes32[] memory keywords,
+    string memory documentType,
+    string memory uri
+  ) external onlyOwner {
+    metadata[ptrTokenId] = Metadata({
+      visibility: visibility,
+      timestamp: block.timestamp,
+      documentHash: documentHash,
+      keywords: keywords,
+      documentType: documentType,
+      uri: uri
+    });
+    _mint(to, ptrTokenId);
+    unchecked { ptrTokenId++; }
+  }
+
+  function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    return metadata[tokenId].uri;
+  }
+
+  // Override _update to prevent sending tokens to other addresses
+  function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+    if (to != address(0) && auth != address(0)) {
+      revert Unauthorized();
+    }
+    return super._update(to, tokenId, auth);
+  }
+}
