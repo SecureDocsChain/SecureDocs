d1972aef22fc566b1c1df4fe26f727572fb86057
chixx <lucottejeremie@gmail.com>
2024-05-27 18:07:05 +0200

    add libraries Error & Struct

---
+++ Backend/src/lib/Errors.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+error Unauthorized();
+error AlreadyInitialized();
+error ContractLogicAlreadySet();
+error SecureVaultAlreadyDeployed();
+error VerifierAlreadyExist();
