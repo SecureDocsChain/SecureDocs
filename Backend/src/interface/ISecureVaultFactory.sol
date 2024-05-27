5b8fa9ab566a5e5c67073de8540642d1857b1b16
chixx <lucottejeremie@gmail.com>
2024-05-27 19:04:22 +0200

    update interface ISecureVaultFactory

--- Backend/src/interface/ISecureVaultFactory.sol
+++ Backend/src/interface/ISecureVaultFactory.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
+import {Verifier} from "../lib/Struct.sol";
+
 interface ISecureVaultFactory {
   function deploy(address owner) external;
-  
+
   function getSecureVault(address owner) external view returns (address);
+
+  function registerVerifier(address verifier, string memory name) external;
+
+  function mint(
+    address user,
+    address verifier,
+    uint8 visibility,
+    bytes32 documentHash,
+    bytes32[] memory keywords,
+    string memory documentType,
+    string memory uri
+  ) external;
+
+  function getVerifier(address verifier) external view returns (Verifier memory);
 }
